4c193d03ecc406a209623e24cdb646693d6713fb
Hernan Lorenzi <hernan.lorenzi@nih.gov>
2022-06-23 21:20:22 -0400

    adding R scripts

--- aux_functions.R
+++ aux_functions.R
@@ -2,24 +2,29 @@
 # It simulates a log transformation of the x or y scale so you can have negative log scales.
 # Use like scale_y_continuous(trans=symlog_trans(thr = 0.015)) or scale_y_continuous(trans="symlog")
 symlog_trans <- function(base = 10, thr = 1, scale = 1){
-  trans <- function(x)
+  trans <- function(x){
     ifelse(abs(x) < thr, x, sign(x) * 
              (thr + scale * suppressWarnings(log(sign(x) * x / thr, base))))
+  }
   
-  inv <- function(x)
+  inv <- function(x){
     ifelse(abs(x) < thr, x, sign(x) * 
              base^((sign(x) * x - thr) / scale) * thr)
+  }
   
   breaks <- function(x){
     sgn <- sign(x[which.max(abs(x))])
-    if(all(abs(x) < thr))
+    if(all(abs(x) < thr)){
       pretty_breaks()(x)
+      }
     else if(prod(x) >= 0){
-      if(min(abs(x)) < thr)
+      if(min(abs(x)) < thr){
         sgn * unique(c(pretty_breaks()(c(min(abs(x)), thr)),
                        log_breaks(base)(c(max(abs(x)), thr))))
-      else
+      }
+      else {
         sgn * log_breaks(base)(sgn * x)
+        }
     } else {
       if(min(abs(x)) < thr)
         unique(c(sgn * log_breaks()(c(max(abs(x)), thr)),
